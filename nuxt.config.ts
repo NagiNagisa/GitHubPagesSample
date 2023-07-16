585ecaf32dcc9cef50be3663f69e03653eecd18e
Nagi Nagisa <nagi_nagisa111@yahoo.co.jp>
2023-07-17 00:25:26 +0900

    fix favicon path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    baseURL: '/GitHubPagesSample/',
-  },
-  devtools: { enabled: true }
+    app: {
+        baseURL: '/GitHubPagesSample/',
+        head: {
+            link: [
+                {rel: 'icon', type: "image/x-icon", href: "/GitHubPagesSample/favicon.ico"}
+            ]
+        }
+    },
+    devtools: {enabled: true}
 })
